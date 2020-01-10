c6da3f0b643e2644ce011c1a42f62df0ba1d6aa0
John.Koepi <john.koepi@gmail.com>
2020-01-10 09:20:55 +0200

    attribute-list allows complex forms

--- parse_attribute.c
+++ parse_attribute.c
@@ -10,7 +10,7 @@
 //
 // Does traceevent support GCC type attributes?
 int main() {
-  struct tep_handle tep = {.handlers={NULL}};
+  struct tep_handle tep = {.handlers=NULL};
   struct tep_event *ev;
   char *buf =
     "name: sys_enter_wait4\n"
@@ -21,7 +21,7 @@ int main() {
     "	field:unsigned char common_preempt_count;	offset:3;	size:1;	signed:0;\n"
     "	field:int common_pid;	offset:4;	size:4;	signed:1;\n"
     "\n"
-    "	field:struct rusage __attribute__((user)) * ru;	offset:40;	size:8;	signed:0;\n"
+    "	field:struct rusage __attribute__ ( ( user,blah, ah( \"ups())\\\" ))()\", 1, 2), __q)) * ru;	offset:40;	size:8;	signed:0;\n"
     "\n"
     "print fmt: \"\"";
   unsigned long size = strlen(buf);
@@ -35,4 +35,10 @@ int main() {
   }
 
   return err;
+}
+
+void f(int __attribute__ (
+      ( user,user, user(1,2,3, "a"), blah(
+          "blah()()(\"())") ) ) a) {
+  printf("%d", a);
 }
